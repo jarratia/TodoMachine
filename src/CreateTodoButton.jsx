1f27b8c669f2e575f8e968257220c841f59ffe5d
jarratia <jarratia34@gmail.com>
2022-01-22 18:03:47 -0300

    add state

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -1,9 +1,12 @@
 import './CreateTodoButton.css'
 
 const CreateTodoButton = () => {
+    const onClickButton = (msg) => {
+        console.log(msg)
+    }
     return (
         <div>
-            <button className='CreateTodoButton' onClick={() => console.log("click")}>
+            <button className='CreateTodoButton' onClick={() => onClickButton("Diste click")}>
                 +
             </button>
         </div>
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,15 +1,19 @@
 import './TodoItem.css';
 
 function TodoItem(props) {
+
+    const onComplete = () => { console.log("ToDo completado") }
+    const onDelete = () => { console.log("Borraste el ToDO") }
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`} onClick={onComplete}>
                 √
             </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <span className="Icon Icon-delete">
+            <span className="Icon Icon-delete" onClick={onDelete}>
                 X
             </span>
         </li>
