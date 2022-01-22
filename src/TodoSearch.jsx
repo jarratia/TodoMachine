8e1820deabaec7e9806f4d9350dd1d4d71497c8e
jarratia <jarratia34@gmail.com>
2022-01-22 18:28:15 -0300

    search state

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -2,6 +2,7 @@ import './TodoSearch.css';
 
 function TodoSearch() {
     const onSearchValueChange = (event) => {
+        const [onSearchValue, setSearchValue] = useState('');
         console.log(event.target.value)
     }
     return (
